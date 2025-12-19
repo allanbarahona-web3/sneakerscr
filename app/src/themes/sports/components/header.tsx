98921264d909a185d92513dc083a525e17496949
Allan Barahona <abarahona@barmentech.com>
2025-12-18 18:34:05 -0600

    fix: remove unused Image import from header

--- app/src/themes/sports/components/header.tsx
+++ app/src/themes/sports/components/header.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Link from 'next/link';
-import Image from 'next/image';
 import { ShoppingBag, Menu, X, ChevronDown } from 'lucide-react';
 import { useState } from 'react';
 import { useSportsCart } from '../context/CartContext';
